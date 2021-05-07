095ce8e3c3395e150857ffef79dafb0b166a9739
maplepancakes <ngsiohongssg@gmail.com>
2021-05-07 20:50:24 +0800

    Addition of code that loads contact page to user display

---
+++ src/contact.js
@@ -0,0 +1,78 @@
+const contactPage = (function()
+{
+    const loadContactPage = function()
+    {
+        const numberOfArrays = 2;
+
+        const contactTitle =
+        [
+            `Phone`,
+            `Address`
+        ]
+
+        const contactDescription = 
+        [
+            `951-572-2602`,
+            `Division 19 Calaveras, CA 95221`
+        ]
+
+        let contactTitleArray = [];
+        let contactDescriptionArray = [];
+
+        const center = document.querySelector(`center`);
+
+        const contactBox = document.createElement(`div`);
+        const contactImage = document.createElement(`img`);
+        const separatorVertical = document.createElement(`div`);
+        const contactDescriptionContainer = document.createElement(`div`);
+
+        for (let i = 0; i < numberOfArrays; i++)
+        {
+            contactTitleArray[i] = document.createElement(`label`);
+            contactDescriptionArray[i] = document.createElement(`label`);
+        }
+
+        contactBox.classList.add(`contact-box`);
+
+        contactImage.classList.add(`menu-image`);
+        contactImage.setAttribute(`src`, `../images/aerial-view-painting.resized.jpg`);
+
+        separatorVertical.classList.add(`separator-vertical`);
+
+        contactDescriptionContainer.classList.add(`menu-description-container`);
+
+        for (let i = 0; i < numberOfArrays; i++)
+        {
+            contactTitleArray[i].classList.add(`label-description`);
+            contactTitleArray[i].classList.add(`menu-title`);
+            contactTitleArray[i].textContent = contactTitle[i];
+
+            contactDescriptionArray[i].classList.add(`label-description`);
+            contactDescriptionArray[i].textContent = contactDescription[i];
+        }
+
+        center.appendChild(contactBox);
+        contactBox.appendChild(contactImage);
+        contactBox.appendChild(separatorVertical);
+        contactBox.appendChild(contactDescriptionContainer);
+        
+        for (let i = 0; i < numberOfArrays; i++)
+        {
+            contactDescriptionContainer.appendChild(contactTitleArray[i]);
+            contactDescriptionContainer.appendChild(contactDescriptionArray[i]);
+        }
+    }
+
+    const unloadContactPage = function()
+    {
+        const center = document.querySelector(`center`);
+        const contactBox = document.querySelector(`.contact-box`);
+
+        center.removeChild(contactBox);
+    }
+
+    return {loadContactPage, unloadContactPage};
+
+{}})();
+
+export default contactPage
